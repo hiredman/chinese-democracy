36a8e7823624ddcdbc96413564cbe69b66d338ff
Kevin Downey (hiredman) <redchin@gmail.com>
2011-04-11 13:56:33 -0700

    release 1.0.0

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject chinese-democracy "0.0.1-SNAPSHOT"
-  :description "FIXME: write"
+(defproject chinese-democracy "1.0.0"
+  :description "master election protocol"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [commons-codec "1.4"]]
   :dev-dependencies [[swank-clojure "1.2.1"]]
