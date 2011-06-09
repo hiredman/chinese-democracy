2edfcc3a56a78b840c8d71e0995a99da2158ec43
Kevin Downey (hiredman) <redchin@gmail.com>
2011-06-09 14:43:34 -0700

    ready for more

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject chinese-democracy "1.0.0"
+(defproject chinese-democracy "2.0.0-SNAPSHOT"
   :description "master election protocol"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [commons-codec "1.4"]]
