8b5f3765ba527d15ad49c311f25b91fff6c668f6
Paul Stadig <paul@stadig.name>
2011-07-19 22:46:02 +0800

    Bump to latest stable Clojure.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject chinese-democracy "1.0.1"
   :description "master election protocol"
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.2.1"]
                  [commons-codec "1.4"]]
   :dev-dependencies [[swank-clojure "1.2.1"]]
   :main chinese.democracy)
