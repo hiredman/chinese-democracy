20ede7f0625d1d247ae3330f72e203bd11d05368
Paul Stadig <paul@stadig.name>
2011-07-20 04:31:49 +0800

    Bump version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.pjstadig/chinese-democracy "1.0.2"
+(defproject org.clojars.pjstadig/chinese-democracy "1.2.0"
   :description "master election protocol"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [commons-codec "1.4"]]
